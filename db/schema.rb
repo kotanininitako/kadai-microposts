203e23fddac431dd6c79929c25f3cf155b9dd136
ryo <ryo.kot.2403@gmail.com>
2022-12-11 10:48:00 +0000

    post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_05_075551) do
+ActiveRecord::Schema.define(version: 2022_12_10_155757) do
+
+  create_table "microposts", charset: "utf8mb4", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -20,4 +28,5 @@ ActiveRecord::Schema.define(version: 2022_12_05_075551) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "microposts", "users"
 end
